3e00e306e09227b657dcc690633cf5ce8abe5889
shturkhal <107053994+shturkhal@users.noreply.github.com>
2023-12-23 11:58:52 +0200

    Initial Commit

---
+++ testProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testProject
+//
+//  Created by Іван Штурхаль on 23.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
